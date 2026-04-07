8b35f7b3377addf5c0e8ea231eb6d6d48827f813
sifat-arch <sifatullah1004@gmail.com>
2026-04-07 16:55:35 +0600

    initial commit for uvideo

---
+++ src/lib/db.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error("Please add your Mongo URI to .env.local");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    mongoose.connect(MONGODB_URI).then(() => mongoose.connection);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    throw error;
+  }
+
+  return cached.conn;
+}
