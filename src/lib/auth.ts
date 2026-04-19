8d6a82425eec8c00637405d0eea8958aa96b751d
sifat-arch <sifatullah1004@gmail.com>
2026-04-19 10:48:17 +0600

    adding next auth logic for signin an signup

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,21 +1,49 @@
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { dbConnect } from "./db";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
 
 export const authOptions: NextAuthOptions = {
   providers: [
-     CredentialsProvider({
-         name:"Credentials",
-         
-         credentials: {
-            email:{label:"Email", type:"text"},
-            password:{label:"Password" , type:"password"}
-         },
+    CredentialsProvider({
+      name: "Credentials",
 
-         async authorize(credentials){
-             if(!credentials?.email || !credentials?.password){
-                 throw new Error("Missing email or password")
-             }
-         }
-     })
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Missing email or password");
+        }
+
+        try {
+          await dbConnect();
+          const user = await User.findOne({ email: credentials.email });
+
+          if (!user) {
+            throw new Error("No user found whth this email");
+          }
+
+          const isValid = await bcrypt.compare(
+            credentials.password,
+            user.password,
+          );
+          if (!isValid) {
+            throw new Error("Inviled Password");
+          }
+
+          return {
+            id: user._id.toString(),
+            email: user.email,
+          };
+        } catch (error) {
+          console.error("Auth Error");
+          throw error;
+        }
+      },
+    }),
   ],
 };
--- src/models/User.ts
+++ src/models/User.ts
@@ -28,4 +28,3 @@ userSchema.pre("save", async function (next) {
 const User = models.User || model<IUser>("User", userSchema);
 
 export default User;
-d
